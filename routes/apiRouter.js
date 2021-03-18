85e250ddc15cb67b04c231273375728b87286915
Alexander Fok <foktmalexander@gmail.com>
2021-03-17 21:50:31 -0700

    Add init apiRouter code

---
+++ routes/apiRouter.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/workouts', (req, res) => {
+
+});
+
+router.put('/workouts', (req, res) => {
+
+});
+
+router.post('/workouts', (req, res) => {
+
+});
+
+router.get('/workouts/range', (req, res) => {
+    
+})
+
+module.exports = router;
