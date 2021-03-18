9b463e4b2189435973ee4a03edd9ca1496dffea3
Alexander Fok <foktmalexander@gmail.com>
2021-03-18 02:40:58 -0700

    Add html router

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const htmlRouter = require('./routes/htmlRouter');
 const apiRouter = require('./routes/apiRouter');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use('/', htmlRouter);
 app.use('/api', apiRouter);
 
 const PORT = process.env.PORT || 3000;
