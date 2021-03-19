c6f6b1fb4302070b54d3cdf70857e9bdc0d96566
Alexander Fok <foktmalexander@gmail.com>
2021-03-18 18:46:07 -0700

    Add in database-related logic for api requests

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,20 +1,40 @@
-const express = require('express');
-const router = express.Router();
+const db = require('../models');
+const router = require('express').Router();
 
-router.get('/workouts', (req, res) => {
-
+router.get('/workouts', async (req, res) => {
+  const workouts = await db.Workout.find({});
+  res.json(workouts);
 });
 
-router.put('/workouts', (req, res) => {
-
+router.put('/workouts/:id', async (req, res) => {
+  const workoutObj = await db.Workout.findOne({ _id: req.params.id });
+  if (workoutObj) {
+    workoutObj.exercises.push(req.body);
+    await workoutObj.save();
+    res.json(workoutObj);
+  } else {
+    console.log("No result found")
+  }  
 });
 
-router.post('/workouts', (req, res) => {
-
+router.post('/workouts', async (req, res) => {
+  const workoutObj = await db.Workout.create(req.body);
+  console.log(req.body);
+  res.json(workoutObj);
 });
 
-router.get('/workouts/range', (req, res) => {
-    
-})
+router.get('/workouts/range', async (req, res) => {
+  const workouts = await db.Workout.aggregate([
+    { $sort: { day: -1, _id: 1 } },
+    { $limit: 7 },
+    { $addFields: {
+      totalDuration: {
+        $sum: '$exercises.duration'
+      }
+    }}
+  ]);
+  console.log(`Number of results: ${workouts.length}`);
+  res.json(workouts);
+});
 
 module.exports = router;
